30f984a9c8a5b022b1cc89e915134f3c341b95b3
alvaro <kulo56534@gmail.com>
2024-11-20 18:51:46 +0100

    aniado formulario con validaciones específicas con ValidationSchema

---
+++ src/modules/registro/views/RegistroView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>Formulario de registro</h2>
+    <Form :validation-schema="schema" @submit="onSubmit()">
+      <div class="form">
+        <label for="nombre">Nombre: </label>
+        <Field
+          type="text"
+          name="nombre"
+          id="nombre"
+          placeholder="Ingrese su nombre"
+        />
+        <ErrorMessage name="nombre"></ErrorMessage>
+      </div>
+      <div class="form">
+        <label for="email">E-mail: </label>
+        <Field
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Ingrese su e-mail"
+        />
+        <ErrorMessage name="email"></ErrorMessage>
+      </div>
+      <div class="form"><button type="submit">Registrar</button></div>
+    </Form>
+  </div>
+</template>
+<script setup>
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { schema } from "../schemas/ValidationSchema";
+
+const onSubmit = () => {
+  console.log("Se ha enviado el formulario");
+};
+</script>
+<style scoped>
+.form {
+  margin-bottom: 10px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,23 +3,24 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    /*   {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    }, */
     {
       path: "/lista-de-tareas",
       name: "Lista de Tareas",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../components/ListaDeTareas.vue"),
+      component: () =>
+        import("../modules/listaDeTareas/components/ListaDeTareas.vue"),
     },
     {
       path: "/contador",
       name: "contador",
-      component: () => import("../components/Contador.vue"),
+      component: () => import("../modules/contador/components/Contador.vue"),
+    },
+    {
+      path: "/registro",
+      name: "registro",
+      component: () => import("../modules/registro/views/RegistroView.vue"),
     },
   ],
 });
